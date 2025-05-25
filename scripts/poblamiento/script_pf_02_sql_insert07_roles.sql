4a743718828456bcd62ebcc8194da75be1ab7dc6
valeriesofia05 <cardenasvalerie61@gmail.com>
2025-05-25 17:11:42 -0500

    Add files via upload

---
+++ scripts/poblamiento/script_pf_02_sql_insert07_roles.sql
@@ -0,0 +1,11 @@
+INSERT INTO roles (id_rol, nombre_rol) VALUES
+(1, 'Administrador'),
+(2, 'Estudiante'),
+(3, 'Docente'),
+(4, 'Evaluador Interno'),
+(5, 'Evaluador Externo'),
+(6, 'Coordinador de Programa'),
+(7, 'Jefe de Departamento'),
+(8, 'Secretario Académico'),
+(9, 'Director de Facultad'),
+(10, 'Invitado');
