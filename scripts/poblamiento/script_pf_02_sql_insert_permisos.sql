3159524cc22edc1a8a782a5cfdc59e75643a433b
valeriesofia05 <cardenasvalerie61@gmail.com>
2025-05-25 17:11:07 -0500

    Delete scripts/poblamiento directory

--- scripts/poblamiento/script_pf_02_sql_insert_permisos.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO permisos (id_permiso, descripcion_permiso) VALUES
-(1, 'Crear, editar, eliminar y visualizar proyectos'),
-(2, 'Crear, editar, eliminar y visualizar usuarios'),
-(3, 'Crear, editar, eliminar y visualizar asignaturas'),
-(4, 'Asignar roles y permisos, administrar configuraciones'),
-(5, 'Evaluar proyectos, gestionar entregables y generar reportes');
